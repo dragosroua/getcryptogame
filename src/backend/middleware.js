fdb0ab8c073c252c8c0d578f6dba2653a38670f4
Dragos Roua <dragosroua@gmail.com>
2022-03-09 07:21:08 -0500

    apply changes

---
+++ src/backend/middleware.js
@@ -0,0 +1,43 @@
+import { createGameDeck, getCurrentGameDeck } from './functions'
+
+/**
+{
+    players: [
+        {
+            id: "player0",
+            name: "Dragos",
+            avatar: "img",
+            cards: {
+                hand: ["keys","wallet", "q13"],
+                portfolio: {
+                    cards: ["ETH", "ADA", "SHIT", "SHIT"]
+                    wallets: [
+                        ["BTC"],
+                        ["ETH", "UNI"]
+                    ]
+                }
+            }
+        },
+        {
+            id: "player0",
+            name: "Eva",
+            avatar: "img",
+            cards: {
+                hand: ["q20","seed", "q08"],
+                portfolio: {
+                    cards: ["ETH", "ATOM", "SHIT", "SHIT"]
+                    wallets: [
+                        ["FLOW"]
+                    ]
+                }
+            }
+        },
+    ],
+    isMe: "player1",
+    isPlaying: "player0",
+}
+ */
+
+export function getPlayerData() {
+  //var currentGameDeck =
+}
