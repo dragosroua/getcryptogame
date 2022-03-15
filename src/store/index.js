c4d2233478de349e4ed40cf82174357df96b82aa
Dragos Roua <dragosroua@gmail.com>
2022-03-15 11:39:00 -0400

    add playedCoinCardsArray to the state

--- src/store/index.js
+++ src/store/index.js
@@ -13,6 +13,7 @@ const store = createStore({
       eventCardsArray: [],
       currentPlayer: {},
       playedEventCardsArray: [],
+      playedCoinCardsArray: [],
     },
     users: [],
     isLoggedIn: false,
